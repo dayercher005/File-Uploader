cb53f6e137a7eb76b422ebde59b05e6f93cde01f
Dayer <dayerxr.cher@gmail.com>
2026-01-19 22:44:28 +0800

    fix: fixed issue of local strategy not syncing with form due to incorrect name with strategy

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,56 +1,49 @@
 import { Strategy as LocalStrategy } from 'passport-local';
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../generated/prisma/client.ts';
+import { prisma } from '../../lib/prisma.ts';
 import passport from 'passport';
-const connectionString = `${process.env.DATABASE_URL}`
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
 import bcrypt from 'bcryptjs';
 
-   
-export function PassportConfiguration(passport: any ){
 
-    passport.use(
-        new LocalStrategy({usernameField: 'username'}, async (username:string, password:string, done: any) => {
-            try{
-                const user = await prisma.user.findUnique({
-                    where: {
-                        username: username,
-                    }
-                })
-
-                if (!user){
-                    return done(null, false, {message: "Incorrect Username"})
-                }
-
-                const match = await bcrypt.compare(password, user.password);
-                if (!match){
-                    return done(null, false, {message: "Incorrect password"})
-                }
-                return done(null, user);
-
-            } catch(error){
-                return done(error)
-            }
-        })
-    )
-
-    passport.serializeUser((member:any, done:any) => {
-        done(null, member.id);
-    })
-
-    passport.deserializeUser( async (id: number, done:Function) => {
+passport.use(
+    new LocalStrategy(({usernameField: 'email'}), async (email:string, password:string, done: any) => {
         try{
             const user = await prisma.user.findUnique({
                 where: {
-                    id: id,
+                    email: email,
                 }
             })
-            done(null, user);
+
+            if (!user){
+                return done(null, false, {message: "Incorrect Username"})
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if (!match){
+                return done(null, false, {message: "Incorrect password"})
+            }
+            return done(null, user);
+
         } catch(error){
-            done(error)
+            return done(error)
         }
     })
+)
 
-}
+passport.serializeUser((member:any, done:any) => {
+    done(null, member.id);
+})
+
+passport.deserializeUser( async (id: string, done:any) => {
+    try{
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id,
+            }
+        })
+        done(null, user);
+    } catch(error){
+        done(error)
+    }
+})
+

d8e5c8d83eca14c6978e573850baa19c637beb10
Dayer <dayerxr.cher@gmail.com>
2026-01-20 15:14:55 +0800

    feat: added new create folder form and private routing for create folder form and dashboard page

--- src/routes/Dashboard/dashboard.ts
+++ src/routes/Dashboard/dashboard.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { renderDashboardPage } from '../../controllers/Dashboard/dashboard.ts';
+import { authenticateRoute } from '../../config/authenticateRoute.ts';
 
 export const DashboardRouter = Router();
 
-DashboardRouter.get("/", renderDashboardPage);
+DashboardRouter.get("/", authenticateRoute, renderDashboardPage);

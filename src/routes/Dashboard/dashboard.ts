a31773a6ae382bf767ea54bb2af1aa14991733ac
Dayer <dayerxr.cher@gmail.com>
2026-01-20 17:21:13 +0800

    feat: added more dynamic routing for individual folders and files

--- src/routes/Dashboard/dashboard.ts
+++ src/routes/Dashboard/dashboard.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
-import { renderDashboardPage } from '../../controllers/Dashboard/dashboard.ts';
+import { renderDashboardPage } from '../../controllers/Dashboard/renderDashboardPage.ts';
+import { renderIndividualFolder } from '../../controllers/Dashboard/renderIndividualFolder.ts';
+import { renderIndividualFile } from '../../controllers/Dashboard/renderIndividualFile.ts';
 import { authenticateRoute } from '../../config/authenticateRoute.ts';
 
 export const DashboardRouter = Router();
 
 DashboardRouter.get("/", authenticateRoute, renderDashboardPage);
+DashboardRouter.get("/:folder", authenticateRoute, renderIndividualFolder);
+DashboardRouter.get("/:folder/:file", authenticateRoute, renderIndividualFile);

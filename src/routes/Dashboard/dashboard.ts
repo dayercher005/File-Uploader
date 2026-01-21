2ba10d7dbb992eff110b48e15feebd241f664019
Dayer <dayerxr.cher@gmail.com>
2026-01-21 13:25:58 +0800

    feat: adding dynamic routes for creating file form

--- src/routes/Dashboard/dashboard.ts
+++ src/routes/Dashboard/dashboard.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { renderDashboardPage } from '../../controllers/Dashboard/renderDashboardPage.ts';
 import { renderIndividualFolder } from '../../controllers/Dashboard/renderIndividualFolder.ts';
 import { renderIndividualFile } from '../../controllers/Dashboard/renderIndividualFile.ts';
+import { renderCreateFileForm } from '../../controllers/CreateFile/renderCreateFileForm.ts';
+import { sendCreateFileForm } from '../../controllers/CreateFile/sendCreateFileForm.ts';
 import { authenticateRoute } from '../../config/authenticateRoute.ts';
 
 export const DashboardRouter = Router();
@@ -9,3 +11,5 @@ export const DashboardRouter = Router();
 DashboardRouter.get("/", authenticateRoute, renderDashboardPage);
 DashboardRouter.get("/:folder", authenticateRoute, renderIndividualFolder);
 DashboardRouter.get("/:folder/:file", authenticateRoute, renderIndividualFile);
+DashboardRouter.get("/:folder/create-file", authenticateRoute, renderCreateFileForm);
+DashboardRouter.post("/:folder/create-file", sendCreateFileForm);
